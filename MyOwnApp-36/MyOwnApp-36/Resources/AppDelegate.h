bb7a6921d37737698d3753a27b54458a3f9624ba
Shean Bjoralt <shean@datamanifold.com>
2020-10-01 14:20:51 -0600

    initial commit

---
+++ MyOwnApp-36/MyOwnApp-36/Resources/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MyOwnApp-36
+//
+//  Created by Shean Bjoralt on 10/1/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MyOwnApp-36/MyOwnApp-36/Resources/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  MyOwnApp-36
+//
+//  Created by Shean Bjoralt on 10/1/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
